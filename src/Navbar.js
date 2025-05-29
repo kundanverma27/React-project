4a6ca49352e34344f29df4721cd7768a0f974c54
kundanverma27 <KUNDANVERMA69498@GMAIL.COM>
2025-05-29 10:10:45 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <nav className="navbar">
-  <a href="#home">Home</a>
+  <a href="/">Home</a>
   <a href="#about">About</a>
   <a href="#services">Services</a>
   <a href="#contact">Contact</a>
